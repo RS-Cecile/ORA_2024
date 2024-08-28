355c1ad8d6abccffee34064cf6f3c2e11a39faae
RS-Cecile <cecilebazin@orange.fr>
2024-08-28 10:38:15 +0200

    Update Par_territoires.py

--- pages/Par_territoires.py
+++ pages/Par_territoires.py
@@ -21,11 +21,94 @@ st.sidebar.markdown("# Par territoires")
 fichier = "ORA_donnee.xlsx"
 sheet = "Territoire"
 
+# Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?
+table = pd.read_excel( fichier, sheet_name = sheet ,skiprows=9,nrows= 9, index_col =0, dtype = "object")
+table = table.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table = table.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#Les économies d'énergie (électricité, gaz,...) et de la ressource en eau
+table1 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=87,nrows= 5, index_col =0, dtype = "object")
+table1 = table1.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table1 = table1.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)
+table2 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=95,nrows= 5, index_col =0, dtype = "object")
+table2 = table2.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table2 = table2.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)
+table3 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=103,nrows= 5, index_col =0, dtype = "object")
+table3 = table3.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table3 = table3.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#Des achats responsables (en local, circuit-court...)
+table4 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=111,nrows= 5, index_col =0, dtype = "object")
+table4 = table4.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table4 = table4.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)
+table5 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=119,nrows= 5, index_col =0, dtype = "object")
+table5 = table5.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table5 = table5.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale
+table6 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=127,nrows= 5, index_col =0, dtype = "object")
+table6 = table6.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table6 = table6.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
+#La sobriété numérique (utilisation durable et raisonnable du numérique)
+table7 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=135,nrows= 5, index_col =0, dtype = "object")
+table7 = table7.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table7 = table7.applymap(lambda x: f'{x * 100:.0f}%')
+
+# Qu'est-ce qui pourrait aider votre association à [mieux] prendre en compte les enjeux liés à la transition écologique dans ses activités et son fonctionnement ? Plusieurs réponses possibles
+table8 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=147,nrows= 9, index_col =0, dtype = "object")
+table8 = table8.rename(columns = {"En ZFRR" : "En ZFRR*"})
+table8 = table8.applymap(lambda x: f'{x * 100:.0f}%')
+
+styled_table = table.style.set_properties(**{'text-align': 'center'})
+styled_table1 = table1.style.set_properties(**{'text-align': 'center'})
+styled_table2 = table2.style.set_properties(**{'text-align': 'center'})
+styled_table3 = table3.style.set_properties(**{'text-align': 'center'})
+styled_table4 = table4.style.set_properties(**{'text-align': 'center'})
+styled_table5 = table5.style.set_properties(**{'text-align': 'center'})
+styled_table6 = table6.style.set_properties(**{'text-align': 'center'})
+styled_table7 = table7.style.set_properties(**{'text-align': 'center'})
+styled_table8 = table8.style.set_properties(**{'text-align': 'center'})
 
 
-tab1, tab2, tab3 = st.tabs(["Par territoire", "Ensemble des territoires", "Téléchargement des données"])
+tab1, tab2, tab3 = st.tabs(["Ensemble des territoires","Par territoire",  "Téléchargement des données"])
 
 with tab1:
+    st.header("Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?")
+    st.table(styled_table)
+    "\* Anciennement dans une zone de revitalisation rurale désormais appelée **France Ruralités Revitalisation** (ZFRR)"
+    st.header("Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?")
+    st.subheader("Les économies d'énergie (électricité, gaz,...) et de la ressource en eau")
+    st.table(styled_table1)
+    st.subheader("La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)")
+    st.table(styled_table2)
+    st.subheader("La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)")
+    st.table(styled_table3)
+    st.subheader("Des achats responsables (en local, circuit-court...)")
+    st.table(styled_table4)
+    st.subheader("Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)")
+    st.table(styled_table5)
+    st.subheader("Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale")
+    st.table(styled_table6)
+    st.subheader("La sobriété numérique (utilisation durable et raisonnable du numérique)")
+    st.table(styled_table7)
+    st.header("Qu'est-ce qui pourrait aider votre association à [mieux] prendre en compte les enjeux liés à la transition écologique dans ses activités et son fonctionnement ? *Plusieurs réponses possibles*")
+    st.table(styled_table8)
+    
+with tab2:
     option = st.selectbox(
         "**Veuillez sélectionner le territoire :**",
         ("En zone rurale",
@@ -153,93 +236,6 @@ with tab1:
     ax.set_xlabel("Valeurs (en %)")
     st.header("Qu'est-ce qui pourrait aider votre association à [mieux] prendre en compte les enjeux liés à la transition écologique dans ses activités et son fonctionnement ? *Plusieurs réponses possibles*")
     st.pyplot(fig)
-
-
-
-
-# Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?
-table = pd.read_excel( fichier, sheet_name = sheet ,skiprows=9,nrows= 9, index_col =0, dtype = "object")
-table = table.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table = table.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#Les économies d'énergie (électricité, gaz,...) et de la ressource en eau
-table1 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=87,nrows= 5, index_col =0, dtype = "object")
-table1 = table1.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table1 = table1.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)
-table2 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=95,nrows= 5, index_col =0, dtype = "object")
-table2 = table2.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table2 = table2.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)
-table3 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=103,nrows= 5, index_col =0, dtype = "object")
-table3 = table3.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table3 = table3.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#Des achats responsables (en local, circuit-court...)
-table4 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=111,nrows= 5, index_col =0, dtype = "object")
-table4 = table4.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table4 = table4.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)
-table5 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=119,nrows= 5, index_col =0, dtype = "object")
-table5 = table5.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table5 = table5.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale
-table6 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=127,nrows= 5, index_col =0, dtype = "object")
-table6 = table6.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table6 = table6.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
-#La sobriété numérique (utilisation durable et raisonnable du numérique)
-table7 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=135,nrows= 5, index_col =0, dtype = "object")
-table7 = table7.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table7 = table7.applymap(lambda x: f'{x * 100:.0f}%')
-
-# Qu'est-ce qui pourrait aider votre association à [mieux] prendre en compte les enjeux liés à la transition écologique dans ses activités et son fonctionnement ? Plusieurs réponses possibles
-table8 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=147,nrows= 9, index_col =0, dtype = "object")
-table8 = table8.rename(columns = {"En ZFRR" : "En ZFRR*"})
-table8 = table8.applymap(lambda x: f'{x * 100:.0f}%')
-
-styled_table = table.style.set_properties(**{'text-align': 'center'})
-styled_table1 = table1.style.set_properties(**{'text-align': 'center'})
-styled_table2 = table2.style.set_properties(**{'text-align': 'center'})
-styled_table3 = table3.style.set_properties(**{'text-align': 'center'})
-styled_table4 = table4.style.set_properties(**{'text-align': 'center'})
-styled_table5 = table5.style.set_properties(**{'text-align': 'center'})
-styled_table6 = table6.style.set_properties(**{'text-align': 'center'})
-styled_table7 = table7.style.set_properties(**{'text-align': 'center'})
-styled_table8 = table8.style.set_properties(**{'text-align': 'center'})
-
-with tab2:
-    st.header("Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?")
-    st.table(styled_table)
-    "\* Anciennement dans une zone de revitalisation rurale désormais appelée **France Ruralités Revitalisation** (ZFRR)"
-    st.header("Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?")
-    st.subheader("Les économies d'énergie (électricité, gaz,...) et de la ressource en eau")
-    st.table(styled_table1)
-    st.subheader("La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)")
-    st.table(styled_table2)
-    st.subheader("La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)")
-    st.table(styled_table3)
-    st.subheader("Des achats responsables (en local, circuit-court...)")
-    st.table(styled_table4)
-    st.subheader("Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)")
-    st.table(styled_table5)
-    st.subheader("Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale")
-    st.table(styled_table6)
-    st.subheader("La sobriété numérique (utilisation durable et raisonnable du numérique)")
-    st.table(styled_table7)
-    st.header("Qu'est-ce qui pourrait aider votre association à [mieux] prendre en compte les enjeux liés à la transition écologique dans ses activités et son fonctionnement ? *Plusieurs réponses possibles*")
-    st.table(styled_table8)
 
 with tab3 :
     #liste des questions pour l'onglet 2:
