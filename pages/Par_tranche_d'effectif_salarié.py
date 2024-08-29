709e852238d30dc92bd0653e34bcde6a51db8af2
RS-Cecile <cecilebazin@orange.fr>
2024-08-29 16:49:23 +0200

    Update Par_tranche_d'effectif_salarié.py

--- pages/Par_tranche_d'effectif_salarié.py
+++ pages/Par_tranche_d'effectif_salarié.py
@@ -108,6 +108,8 @@ with tab2:
          "10 à 19 salariés",
          "20 à 49 salariés"))
 
+     st.markdown(f'<h1 style = "font-size:30px;"> Vous avez sélectionné : </h1>'f'<h1 style = "font-size:40px;color:royalblue;text-transform:uppercase;"> {option} </h1>', unsafe_allow_html=True)
+
     # Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?
     table_ = pd.read_excel( fichier, sheet_name = sheet ,skiprows=11,nrows= 8, index_col =0, dtype = "object")
     table_ = table_.applymap(lambda x: f'{x * 100:.0f}%')
